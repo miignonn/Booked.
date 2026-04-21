489b80b7091a78c95667df577c48018511484b65
Mignon Pretorius <mignon.2004@gmail.com>
2026-04-21 14:35:19 +0200

    Add auth_check - session guard and role protection

---
+++ includes/auth_check.php
@@ -0,0 +1,20 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE){
+    session_start();
+
+    //checks is session variable exists
+    if(!isset($_SESSION['user_id'])){
+        header("Location: /public/login.php");
+        exit();
+    }
+
+    function require_role(string $role): void{
+         if (!isset($_SESSION['role']) || $_SESSION['role'] !== $role) {
+            header("Location: /public/index.php"); //sends non admin to home page instead of login
+            exit();
+        }
+
+    }
+
+}
