7080e68bb9287bd78f3f87fea52ee5cbba33c82a
Mignon Pretorius <mignon.2004@gmail.com>
2026-05-24 12:37:48 +0200

    Load .env locally, keep system env vars for Render

--- config/db.php
+++ config/db.php
@@ -1,9 +1,15 @@
 <?php
+// Load .env file
+$env = parse_ini_file(__DIR__ . '/../.env');
+foreach ($env as $key => $value) {
+    putenv("$key=$value");
+}
+
 $servername = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
-$port = getenv('DB_PORT') ?: 3306;
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASS');
+$dbname     = getenv('DB_NAME');
+$port       = getenv('DB_PORT') ?: 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
