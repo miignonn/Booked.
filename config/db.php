d3c241c7a51ec61bed79666cedf5de31c73a0a48
Mignon Pretorius <mignon.2004@gmail.com>
2026-05-29 19:09:17 +0200

    Sanitize db connection error, set ubf8mb4 charset

--- config/db.php
+++ config/db.php
@@ -13,5 +13,7 @@ $port       = getenv('DB_PORT') ?: 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("DB connection failed: " . $conn->connect_error);
+    die("Service temporarily unavailable");
 }
+$conn->set_charset('utf8mb4');
