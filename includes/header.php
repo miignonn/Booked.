d6901d5d995c577e195a7004d0466ba4447cd5cc
Mignon Pretorius <mignon.2004@gmail.com>
2026-05-30 15:36:05 +0200

    Fix csrf token no longer overwritten by header.php

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . '/../includes/functions.php';
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+require_once __DIR__ . '/../config/db.php';
 
 if (empty($_SESSION['csrf_token'])){
 $_SESSION['csrf_token'] = bin2hex(random_bytes(32)); 
